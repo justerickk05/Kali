50a758e84f0933b254ae87239b1e024f3a41ed8f
I-ERICK05-I <diegoplay728@gmail.com>
2024-09-15 21:36:51 -0600

    index.css

--- scripts/menuIconToggle.js
+++ scripts/menuIconToggle.js
@@ -1,8 +1,16 @@
 const query = (name) => {return document.querySelector(`${name}`)};
 const menu = query("#menu-icon");
 const navlist = query(".navlist");
+const overlay = query(".overlay")
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
+    overlay.classList.toggle('open')
 };
+
+overlay.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+    overlay.classList.toggle('open')
+}
