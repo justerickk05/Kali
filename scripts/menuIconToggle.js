f70bb1e168caccc1d3bc5cb38c5a87aefe26bf62
I-ERICK05-I <diegoplay728@gmail.com>
2024-09-16 18:04:28 -0600

    header responsive

--- scripts/menuIconToggle.js
+++ scripts/menuIconToggle.js
@@ -1,16 +1,16 @@
 const query = (name) => {return document.querySelector(`${name}`)};
 const menu = query("#menu-icon");
-const navlist = query(".navlist");
+const navbar = query("nav ul");
 const overlay = query(".overlay")
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+    navbar.classList.toggle('open');
     overlay.classList.toggle('open')
 };
 
 overlay.onclick = () => {
     menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+    navbar.classList.toggle('open');
     overlay.classList.toggle('open')
 }
--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -4,7 +4,3 @@ const sr = ScrollReveal ({
     delay: 450,
     reset: false
 });
-
-sr.reveal('.hero-text',{delay:100,origin:'top'});
-sr.reveal('.hero-image',{delay:150,origin:'top'});
-sr.reveal('.scroll-down',{delay:200,origin:'right'});
